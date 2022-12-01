3910d76ac015906132d677cb8ff61656f79881f3
Abhijit Aher <84370735+AbhijitAher@users.noreply.github.com>
2022-12-01 13:37:50 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <QRCodeComponent />
+  </div>
+</template>
+
+<script>
+import QRCodeComponent from "./components/QRCode";
+
+export default {
+  name: "App",
+  components: {
+    QRCodeComponent,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/QRCode.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <h2>VueJS QRCode Generator</h2>
+    <br />
+    <h4>Enter Your Text below to generate the QRCode</h4>
+
+    <textarea
+      class="input"
+      type="text"
+      placeholder="Text"
+      @input="setQRString"
+    />
+
+    <div>
+      <qrcode :value="QRString" :options="{ width: 300 }"></qrcode>
+    </div>
+  </div>
+</template>
+
+<script>
+import VueQrcode from "@chenfengyuan/vue-qrcode";
+// https://github.com/soldair/node-qrcode#qr-code-options
+// https://github.com/fengyuanchen/vue-qrcode/tree/v1
+
+import Vue from "vue";
+
+Vue.component(VueQrcode.name, VueQrcode);
+
+export default {
+  name: "QRCodeComponent",
+
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      QRString: "",
+    };
+  },
+
+  methods: {
+    setQRString(e) {
+      let value = e.target.value;
+      this.QRString = value;
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.input {
+  margin-top: 20px;
+  padding: 10px;
+  border-radius: 5px;
+  height: 100px;
+  width: 300px;
+  border: 2px solid rgb(189, 66, 189);
+}
+</style>
